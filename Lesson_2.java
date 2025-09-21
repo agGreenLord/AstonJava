6c3799875c4164875963bcf1437f1c100820fab0
agGreenLord <a.g.local.mail@gmail.com>
2025-09-21 18:55:44 +0300

    Домашнее задание по классам Создано два файла - Lesson_2 где прописана основная логика программы и методы, а так же внутри файла есть класс park из 3его задания А так же создан файл Product где описан только класс Продукт из задания 1 и 2 Разделил эти два файла чтобы показать что классы необходимо прописывать в отдельном файле и импортировать в main, но тк не знал на сколько это правильно, класс Park прописал внутри main файла

---
+++ Lesson_2.java
@@ -0,0 +1,41 @@
+
+
+public class Lesson_2 {
+    public static void main(String[] args){
+        Product product1 = new Product("Michel", "13.01.2000", "DJI", "USA", 10000, false);
+        product1.infoOfProduct();
+
+        Product[] productArray = new Product[5];
+        productArray[0] = new Product("Samsung S25 Ultra",  "01.02.2025", "Samsung Corp.", "Korea", 5599, true);
+        productArray[1] = new Product("Samsung A5",  "05.02.2020", "Samsung Corp.", "Korea", 500, true);
+        productArray[2] = new Product("Note 12 Ultra",  "01.07.2024", "Xiaomi", "Korea", 5000, false);
+        productArray[3] = new Product("IPhone 17 Pro",  "15.09.2025", "Apple", "USA", 10000, false);
+        productArray[4] = new Product("Iphone 16",  "06.10.2024", "Apple", "USA", 9999, true);
+        productArray[3].infoOfProduct();
+
+        Park park1 = new Park("Woodland");
+        park1.attraction = park1.new Attraction("колесо обозрения", "12-15", 500);
+        System.out.println(park1.attraction.name);
+    }   
+
+    public static class Park {
+        private String parkName;
+        public Attraction attraction;
+
+        public Park (String _name){
+            parkName = _name;
+        }
+        
+        public class Attraction{
+            private String name;
+            private String workTime;
+            private int price;
+            public Attraction(String _name, String _workTime, int _price){
+                name = _name;
+                workTime = _workTime;
+                price = _price;
+            }
+        }
+
+    }
+}
---
+++ Product.java
@@ -0,0 +1,23 @@
+public class Product {
+    private String name;
+           private String dateOfProduction;
+           private String manufacturer;
+           private String countryProduction;
+           private int price;
+           private boolean bookingStatus;
+           
+           public Product(String _name, String _dateOfProduction, String _manufacturer, String _countryProduction, int _price, boolean _bookingStatus){
+               name = _name;
+               dateOfProduction = _dateOfProduction;
+               manufacturer = _manufacturer;
+               countryProduction = _countryProduction;
+               price = _price;
+               bookingStatus = _bookingStatus;
+           }
+            public void infoOfProduct() {
+               System.out.println("Информация об объекте: Название - " + this.name + " Дата производства - "
+               + this.dateOfProduction + " Производитель - " + this.manufacturer + " Страна производитель - " 
+               + this.countryProduction + " Цена - " + this.price + " Статус заказа - " + this.bookingStatus);
+            }
+    
+}
